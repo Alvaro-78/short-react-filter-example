184555eb9e8ceae0b768f7dc918986338643f88d
alvaro-78 <alvaro78an@gmail.com>
2021-11-23 15:13:57 +0100

    Filter example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const people = [
+		{ name: 'Alan Turing', number: '040-123456', id: 1 },
+		{ name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+		{ name: 'Barbara Liskov', number: '12-43-234345', id: 3 },
+		{ name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+	];
+
+	const [search, setNewSearch] = useState('');
+
+	const handleSearchChange = (e) => {
+		setNewSearch(e.target.value);
+	};
+
+	const filtered = !search
+		? people
+		: people.filter((person) =>
+				person.name.toLowerCase().includes(search.toLowerCase())
+		  );
+
+	return (
+		<>
+			<h2>Phone book</h2>
+			Filter persons:{' '}
+			<input type="text" value={search} onChange={handleSearchChange} />
+			<h2>Numbers</h2>
+			{filtered.map((person) => {
+				return (
+					<p key={person.id}>
+						{person.name} - {person.number}
+					</p>
+				);
+			})}
+		</>
+	);
 }
 
 export default App;
